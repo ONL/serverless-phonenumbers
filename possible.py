03be5ad27f68dec2519dfcf9001780c265d2b215
ONL <chinook-media@gmx.de>
2019-02-12 15:50:33 +0100

    Update possible.py

--- possible.py
+++ possible.py
@@ -8,13 +8,19 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/plain')
         self.end_headers()
         
-        if (None == param_num) {
-          param_num = "+12345678"
-        }
+        length = int(self.headers.getheader('content-length'))
+        field_data = self.rfile.read(length)
+        fields = urlparse.parse_qs(field_data)
         
-        if (None == param_country) {
-          param_country = None
-        }
+        if (None != fields["N"]):
+            param_num = fields["N"]
+        else:
+            param_num = "+12345678"
+        
+        if (None != fields["C"]):
+            param_country = fields["C"]
+        else:
+            param_country = None
         
         num = phonenumbers.parse(param_num, param_country)
         message = phonenumbers.is_possible_number(num)

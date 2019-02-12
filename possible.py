142cf7a6384d094246764a6d9b0c60cec9b8ba9b
ONL <chinook-media@gmx.de>
2019-02-12 16:32:35 +0100

    Update possible.py

--- possible.py
+++ possible.py
@@ -23,6 +23,6 @@ class handler(BaseHTTPRequestHandler):
             param_country = None
         
         num = phonenumbers.parse(param_num, param_country)
-        message = phonenumbers.is_possible_number(num)
+        message = str(phonenumbers.is_possible_number(num))
         self.wfile.write(message.encode())
         return

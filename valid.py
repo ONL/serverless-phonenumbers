88af3858b573e3b534fd0be3c4aafb2ab1612e94
ONL <chinook-media@gmx.de>
2019-02-13 10:33:05 +0100

    Update valid.py

--- valid.py
+++ valid.py
@@ -2,27 +2,26 @@ from http.server import BaseHTTPRequestHandler
 import phonenumbers
 
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
         
-        length = int(self.headers.getheader('content-length'))
-        field_data = self.rfile.read(length)
-        fields = urlparse.parse_qs(field_data)
+        #length = int(self.headers.getheader('content-length'))
+        #field_data = self.rfile.read(length)
+        #fields = urlparse.parse_qs(field_data)
         
-        if fields["N"] is not None:
-            param_num = fields["N"]
-        else:
-            param_num = "+12345678"
+        #if fields["N"] is not None:
+        #    param_num = fields["N"]
+        #else:
+        #    param_num = "+12345678"
         
-        if fields["C"] is not None:
-            param_country = fields["C"]
-        else:
-            param_country = None
+        #if fields["C"] is not None:
+        #    param_country = fields["C"]
+        #else:
+        #    param_country = None
         
-        num = phonenumbers.parse(param_num, param_country)
+        #num = phonenumbers.parse(param_num, param_country)
         # message = str(phonenumbers.is_valid_number(num))
         message = "True"
         
